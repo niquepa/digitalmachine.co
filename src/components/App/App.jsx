d61715403b419e30e39eaf43ad6af2119cdcf866
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2018-02-11 23:09:33 -0500

    - custom bootstrap

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import React  from 'react';
 import Header from '../Header/Header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="layout-wide bg-primary" >
-        <Header />
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <div className="layout-wide bg-primary" >
+    <Header />
+  </div>
+)
 
 export default App;
