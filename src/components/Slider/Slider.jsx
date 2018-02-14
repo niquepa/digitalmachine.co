88dfcfe62ea8ae6cda4bfef10ac8ed315fb711f7
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2018-02-13 22:57:07 -0500

    - 1 new image - slider styles

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -10,19 +10,19 @@ import {
 const items = [
   {
     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    src: 'https://images.contentful.com/9kkanfiwhjvs/5UZjYfZwxGmacOC4WY2eKg/d6506953ea572b705460e17e6a0bedb5/pexels-photo-595804.jpeg?w=1440&h=960',
+    src: 'https://images.contentful.com/9kkanfiwhjvs/hK4MpKFX6EKQQSsOAg6kw/316c57c921b795c03d75d9709c7bafa5/pexels-photo-433604.jpeg?w=1440&h=960',
     altText: 'Generando más interacciones con los consumidores, cada vez más simples e inmediatas',
     caption: 'INTEGRAMOS LA TECNOLOGIA EN EL MUNDO FISICO',
   },
   {
     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    src: 'https://images.contentful.com/9kkanfiwhjvs/3gCyvS0nWUUgewkEcyKWq8/ffe2b740d9c4d4ddfc0ce14784dad4bb/pexels-photo-546819.jpeg?w=1440&h=960',
+    src: 'https://images.contentful.com/9kkanfiwhjvs/5UZjYfZwxGmacOC4WY2eKg/d6506953ea572b705460e17e6a0bedb5/pexels-photo-595804.jpeg?w=1440&h=960',
     altText: 'Escuchamos y generamos oportunidades de negocio y marketing más personalizadas',
     caption: 'TENEMOS VISION INTEGRAL DE NUESTROS CLIENTES',
   },
   {
     // src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    src: 'https://images.contentful.com/9kkanfiwhjvs/5aC1EdkSBiAa0gysqqYUU0/1f95e849e8caf14e33e9dd90b5e7126d/pexels-photo-157039.jpeg?w=1440&h=960',
+    src: 'https://images.contentful.com/9kkanfiwhjvs/3gCyvS0nWUUgewkEcyKWq8/ffe2b740d9c4d4ddfc0ce14784dad4bb/pexels-photo-546819.jpeg?w=1440&h=960',
     altText: 'We’re your digital partner, closely collaborating to build an incredible online experience for your users. We combine technical expertise with strategic planning to achieve your business goals.',
     caption: 'WE MAKE WEB AND MOBILE APPS',
   },
