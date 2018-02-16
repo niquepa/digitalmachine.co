fa2d2fa0e05b6e059bdf634fd6df6250c134d03e
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2018-02-16 18:55:56 -0500

    - ServiceItems

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
-import React    from 'react';
-import Header   from '../Header/Header';
-import Slider   from '../Slider/Slider';
-import Services from "../Services/Services";
+import React        from 'react';
+import Header       from '../Header/Header';
+import Slider       from '../Slider/Slider';
+import Services     from "../Services/Services";
+import ServiceItems from "../Services/ServiceItems";
 
 const App = props => (
   <div>
     <Header />
     <Slider />
     <Services/>
+    <ServiceItems/>
   </div>
 );
 
