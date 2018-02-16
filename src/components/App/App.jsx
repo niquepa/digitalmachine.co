6386a7c3d7b174d2f146a4b132fce667f2825eb5
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2018-02-16 10:53:31 -0500

    - Services header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Slider from '../Slider/Slider';
+import React    from 'react';
+import Header   from '../Header/Header';
+import Slider   from '../Slider/Slider';
+import Services from "../Services/Services";
 
 const App = props => (
   <div>
     <Header />
     <Slider />
+    <Services/>
   </div>
 );
 
