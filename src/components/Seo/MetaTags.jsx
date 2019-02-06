6fec7d894bdde198184cd7b5f157e20157b788f0
Carlos Castillo <niquepa@gmail.com>
2019-02-05 22:17:35 -0500

    feat(Twitter card)

--- src/components/Seo/MetaTags.jsx
+++ src/components/Seo/MetaTags.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import { getLocale, urlLocalized } from '../../utils/i18n/i18nInit';
+import { homepage } from '../../../package.json';
 
 class MetaTags extends Component {
   render() {
@@ -20,11 +21,13 @@ class MetaTags extends Component {
           {/* <FbTags locale={locale} location={this.props.location} /> TODO: Helmet doesnt allow Fragments */}
           <meta property="og:site_name" content="DigitalMachine" />
           <meta property="og:type" content="website" />
-          <meta property="og:url" content={window.location.href} />
+          <meta property="og:url" content={`${homepage}${window.location.pathname}`} />
           { title && <meta property="og:title" content={title} /> }
           { description && <meta property="og:description" content={description} /> }
           { image && <meta property="og:image" content={image} /> }
           { video && <meta property="og:video" content={video} /> }
+
+          <meta name="twitter:card" content="summary_large_image" />
         </Helmet>
       </React.Fragment>
     );
@@ -34,7 +37,7 @@ class MetaTags extends Component {
 MetaTags.propTypes = {
   location: PropTypes.object, // TODO: Validate object
   metaData: PropTypes.object, // TODO: Validate object
-  //title: PropTypes.string,
+  title: PropTypes.string,
   description: PropTypes.string,
   image: PropTypes.string,
   video: PropTypes.string,
